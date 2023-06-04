275c544141cf44c4c50cdcd1534b9249931afee3
Md. Jawad Jamil <md.jawadjamil@gmail.com>
2023-06-04 22:49:55 +0600

    Update script.js

--- script.js
+++ script.js
@@ -164,12 +164,11 @@ startBtn.addEventListener("click",()=>{
 	audtg.play()
 })
 
-
+let k = 1;
 if(audtg.currentTime==audtg.duration){
-	for (let i = 1; i < surahs.length; i++) {
 	  audtg.pause();
-	  audtg.setAttribute("src",`https://server8.mp3quran.net/afs/00{i}.mp3`)
+	  audtg.setAttribute("src",`https://server8.mp3quran.net/afs/00${k}.mp3`)
 	  audtg.load();
 	  audtg.play();
-	}
+	  k++;
 })
